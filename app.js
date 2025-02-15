b291b981632805c7c6e9b0d3d6add23422d77939
Malasri32 <malasri.saravanan32@gmail.com>
2025-02-16 01:48:48 +0530

    updated port in server file

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(express.static("public"));
 app.use(express.static('views/assets'));
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
